8d7fbd6408d250f772a8363b8bbb32e4b6358fe5
Grigory Morozov <morozehc@yandex.ru>
2025-11-09 21:37:38 +0300

    feat: set Routes for components app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,20 @@
-import { ConstructorPage, Feed } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import '../../index.css';
 import styles from './app.module.css';
+import { ProtectedRoute } from '../protected-route';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 
 const App = () => (
   <div className={styles.app}>
@@ -11,8 +22,84 @@ const App = () => (
     <Routes>
       <Route path='/' element={<ConstructorPage />} />
       <Route path='/feed' element={<Feed />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRoute>
+            <ResetPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRoute>
+            <ProfileOrders />
+          </ProtectedRoute>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+      {/* модалки */}
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal onClose={() => {}} title=''>
+            <OrderInfo />
+          </Modal>
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal onClose={() => {}} title=''>
+            <IngredientDetails />
+          </Modal>
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <ProtectedRoute>
+            <Modal onClose={() => {}} title=''>
+              <OrderInfo />
+            </Modal>
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   </div>
 );
-
+//  <Route path="/login" element={<ProtectedRoute onlyUnAuth><LoginPage/></ProtectedRoute>} />
 export default App;
