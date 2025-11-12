b17e7576f29cb03ca3939991eaa79f2b10781787
Grigory Morozov <morozehc@yandex.ru>
2025-11-12 18:39:51 +0300

    fear: connect orders to RootReducer users and feed

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import ingredientReducer from './slices/ingredients/ingredientSlice';
 import burgerConsructor from './slices/constructorBurger/constrBurgSlice';
-import  orderSlice  from './slices/orderSlice';
+import orderSlice from './slices/orders/orderSlice';
+import CommonOrders from './slices/orders/commonOrdersSlice';
 
 export const rootReducerBurger = combineReducers({
   ingredients: ingredientReducer,
   constructorBurgers: burgerConsructor,
-  newOrder: orderSlice
+  newOrder: orderSlice,
+  allOrders: CommonOrders
 });
