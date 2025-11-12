beadf750b2bc5363bc27d156960e5289bf4716dd
Grigory Morozov <morozehc@yandex.ru>
2025-11-12 12:45:18 +0300

    feat: added func prepare ing-s - add atr id for every ing(divide on count)

---
+++ src/utils/utilsForArrs.ts
@@ -0,0 +1,49 @@
+// Общие
+import { TConstructorIngredient, TIngredient } from '@utils-types';
+import { IngridientWithChoseCount } from 'src/services/slices/ingredients/ingredientSlice';
+
+//  вернем найденный элемент и его индекс в массиве по ключу элемента(объект)
+export const findElement = <T, K extends keyof T>(
+  arr: T[],
+  key: K,
+  value: T[K]
+): { element: T; index: number } | undefined => {
+  const index = arr.findIndex((item) => item[key] === value);
+  if (index === -1) return;
+  return { element: arr[index], index };
+};
+// фильтруем элементы по типу ключа  напр type=== 'bun', чтобы получить все булки
+export const filterElems = <T, K extends keyof T>(
+  arr: T[],
+  key: K,
+  value: T[K]
+): T[] => arr.filter((item) => item[key] === value);
+//  построим массив элементов без элемента с указанным значение ключа
+// напр id!=2
+//  переделать!!!!!
+export const filterWithoutElem = <T, K extends keyof T>(
+  arr: T[],
+  key: K,
+  value: T[K]
+): T[] => arr.filter((item) => item[key] !== value);
+
+//  касателньо проекта
+
+// создаем id каждому добавленном ингридиенту в корзине
+
+export const preparWithIdIngreds = (
+  ingredients: IngridientWithChoseCount[]
+): TConstructorIngredient[] => {
+  const arr = ingredients.reduce<TConstructorIngredient[]>((acc, item) => {
+    const numberIngr = Number(item.count);
+    if (numberIngr === 0 || !numberIngr) return acc;
+    for (let i = 1; i <= numberIngr; i++) {
+      //  создаем переменную для разделения ингридиентов по id
+      let { count, ...rest } = item;
+      let itemWithId = { ...rest, id: String(i) };
+      acc.push(itemWithId);
+    }
+    return acc;
+  }, []);
+  return arr;
+};
