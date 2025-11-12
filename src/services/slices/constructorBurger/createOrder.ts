c68a8cdc8fe09edc4a0d49fcd055af921f47fded
Grigory Morozov <morozehc@yandex.ru>
2025-11-12 15:23:22 +0300

    feat: create async thunk for creating order

---
+++ src/services/slices/constructorBurger/createOrder.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { dataIngIds, orderBurgerApi } from '@api';
+import { AppDispatch, dispatch, RootState } from 'src/services/store';
+import { prepareToOrder } from './constrBurgSlice';
+
+//  нажмем на кнопку чтобы создать заказ
+export const createOrder = createAsyncThunk(
+  'orders/createOrder', // <слайс>/<действие>
+  async (_, thunkApi) => {
+    try {
+      const state = thunkApi.getState() as RootState;
+      const dispatch = thunkApi.dispatch as AppDispatch;
+      dispatch(prepareToOrder()); // готовим ингридиенты к созданию заказа
+      //  получаем  актуальное состояние id ингридиентов
+      const stateIngredients = (thunkApi.getState() as RootState)
+        .constructorBurgers.idIngredsForOrder;
+      //  создаем заказ и отправляем его на сервер
+      const data = await orderBurgerApi({
+        ingredients: stateIngredients
+      });
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(`Error: ${error}`);
+    }
+  }
+);
+
+// //burgerConstructor/createOrder/pending
+// burgerConstructor/createOrder/fulfilled
+// burgerConstructor/createOrder/rejected
