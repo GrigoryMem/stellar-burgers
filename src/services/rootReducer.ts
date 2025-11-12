29e878bce1b3ca49f92fa9b6a553e09c7f02ec2b
Grigory Morozov <morozehc@yandex.ru>
2025-11-12 12:44:00 +0300

    feat: connect burger slice with store

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import ingredientReducer from './slices/ingredients/ingredientSlice';
+import burgerConsructor from './slices/constructorBurger/constrBurgSlice';
 
 export const rootReducerBurger = combineReducers({
-  ingredients: ingredientReducer
+  ingredients: ingredientReducer,
+  constructorBurgers: burgerConsructor
 });
