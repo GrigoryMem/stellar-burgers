ab0b9548cf942692fffdfe6d9118850f49c21569
Grigory Morozov <morozehc@yandex.ru>
2025-11-12 15:26:07 +0300

    feat: add setTotalsum reducer for constrBurger

--- src/services/slices/constructorBurger/constrBurgSlice.ts
+++ src/services/slices/constructorBurger/constrBurgSlice.ts
@@ -11,11 +11,15 @@ import { IngridientWithChoseCount } from '../ingredients/ingredientSlice';
 type TBurgConstrState = {
   addedIngredients: TIngredient[];
   dividedIngwithId: TConstructorIngredient[];
+  idIngredsForOrder: string[];
+  totalSum: number;
 };
 
 const initialState: TBurgConstrState = {
   addedIngredients: [],
-  dividedIngwithId: []
+  dividedIngwithId: [],
+  idIngredsForOrder: [],
+  totalSum: 0
 };
 
 const burgConstrSlice = createSlice({
@@ -34,8 +38,22 @@ const burgConstrSlice = createSlice({
       if (state.addedIngredients.length > 0) {
         state.dividedIngwithId = divideIngridientsById(state.addedIngredients);
       }
+    },
+    // подготовим добавленные ингридиенты в корзине к созданию заказа
+    prepareToOrder: (state) => {
+      //  положим все id ингридиентов в массив для нашего заказ
+      state.idIngredsForOrder = state.dividedIngwithId.map((item) => item._id);
+    },
+    //  подсчитаем сумму заказа в сторе
+    setTotalSum: (state, action) => {
+      state.totalSum = action.payload;
     }
   }
 });
-export const { setIngredients, divideIngridients } = burgConstrSlice.actions;
+export const {
+  setIngredients,
+  divideIngridients,
+  prepareToOrder,
+  setTotalSum
+} = burgConstrSlice.actions;
 export default burgConstrSlice.reducer;
