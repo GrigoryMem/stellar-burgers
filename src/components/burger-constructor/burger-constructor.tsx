c844bb16b0a25656b30cc95c5b674befa2950a32
Grigory Morozov <morozehc@yandex.ru>
2025-11-14 21:53:43 +0300

    feat: added default bun for cart burger constructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,21 +1,30 @@
 import { FC, useMemo } from 'react';
-import { TConstructorIngredient } from '@utils-types';
+import { TConstructorIngredient, TIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
 import { ingredientsSelector } from '../../services/slices/ingredients/ingredientSlice';
 import { useSelector } from '../../services/store';
 import { filterIngredients } from '../../utils/utils';
 import { ConstructorElement } from '@zlden/react-developer-burger-ui-components';
+import { dividedIngrtsSelector } from '../../services/slices/constructorBurger/constrBurgSlice';
 
 export const BurgerConstructor: FC = () => {
-  const ingredients = useSelector(ingredientsSelector);
-  const { bunsArr, ...restIngs } = filterIngredients(ingredients);
-  // console.log(ingredients);
   /** TODO: взять переменные constructorItems, orderRequest и orderModalData из стора */
+  const allIngedients = useSelector(ingredientsSelector);
+  const { bunsArr: defaultBun, ...useless } = filterIngredients(allIngedients);
+  const addedIngredients = useSelector(dividedIngrtsSelector);
+  //  что добавлено в корзину конструктора
+  const {
+    bunsArr: addedBunArr,
+    mainsArr: addedMainsArr,
+    saucesArr: addedSaucesArr
+  } = filterIngredients(addedIngredients);
+  //  либо с клика либо с всех загруженых ингридиентов либо  дефолт
+  const bunData = addedBunArr[0] ?? defaultBun[0];
   const constructorItems = {
     bun: {
-      price: 0
+      ...bunData
     },
-    ingredients: []
+    ingredients: [...addedMainsArr, ...addedSaucesArr]
   };
 
   const orderRequest = false;
@@ -31,7 +40,7 @@ export const BurgerConstructor: FC = () => {
     () =>
       (constructorItems.bun ? constructorItems.bun.price * 2 : 0) +
       constructorItems.ingredients.reduce(
-        (s: number, v: TConstructorIngredient) => s + v.price,
+        (s: number, v: TConstructorIngredient | TIngredient) => s + v.price,
         0
       ),
     [constructorItems]
