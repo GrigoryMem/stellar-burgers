9f22b2067dbf65d10605f7dc98346349cdbec941
Grigory Morozov <morozehc@yandex.ru>
2025-11-15 16:38:58 +0300

    fix: correct setOrders for get orders with api server

--- src/components/ui/pages/feed/type.ts
+++ src/components/ui/pages/feed/type.ts
@@ -1,7 +1,7 @@
 import { TOrder, TReadyOrder } from '@utils-types';
 
 export type FeedUIProps = {
-  orders: TOrder[] | TReadyOrder[];
+  orders: TOrder[];
   // orders: TOrder[];
   handleGetFeeds: () => void;
 };
