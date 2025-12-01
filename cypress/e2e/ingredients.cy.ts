b566c5f6fce9a2413d1a1faee6a7036332f76f30
Grigory Morozov <morozehc@yandex.ru>
2025-12-01 17:42:40 +0300

    test: create test for adding ingredient in burger-constructor

--- cypress/e2e/ingredients.cy.ts
+++ cypress/e2e/ingredients.cy.ts
@@ -1,13 +1,64 @@
+import store from '../../src/services/store';
+
 describe('ингредиенты', () => {
-  it('Добавление ингредиента из списка ингредиентов в конструктор',() => {
+  beforeEach(() => {
+    //  очищать корзину через стор redux перед каждым тестом
+    store.dispatch({
+      type: 'burgerConstructor/clearConstructor'
+    });
+    // dispatch({type:'burgerConstructor/clearConstructor'});
+    //  делаем перехват запросов
+    // получили юзера
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '**/auth/user'
+      },
+      {
+        fixture: 'test-user-data'
+      }
+    ).as('getUser');
+    // получили ингредиенты
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '**/ingredients'
+      },
+      {
+        fixture: 'test-ingredients'
+      }
+    ).as('getIngredients');
     //  переходим в домашнюю директорию
+  });
+  afterEach(() => {
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
+  });
+  it('Добавление ингредиента из списка ингредиентов в конструктор', () => {
     cy.visit('/');
-    // делаем мок запрос на сервер и получаем ингредиенты
-    
-    // получаем сущности
-
+    // добавим булку
+    cy.get('[data-cy=ingredient]')
+      .filter('[data-type=bun]')
+      .eq(1) // мы знаем что булеи в начале
+      .scrollIntoView() // прокрутка чтобы элемент сталвидимым
+      .within(() => {
+        // ограничисся работойс ним
+        cy.get('button').should('be.visible').click();
+      });
+    // добавляем ингредиет отличный от булки:
+    cy.get('[data-cy=ingredient]')
+      .not('[data-type=bun]')
+      .eq(0) // возьмем второй элмент из коллекции найденных ингредиентов
+      .scrollIntoView() // прокрутка чтобы элемент сталвидимым
+      .within(() => {
+        // ограничисся работойс ним
+        // ищем внутри карточки ингредиента кнопку и когда она станет видима жмем ее
+        cy.get('button').should('be.visible').click();
+      });
     //  кликаем на ингредиент
-
-    // ожидаем что в корзине он появился
+    // ожидаем что в корзине  появился ингредиент отличный от булки
+    cy.get('[data-cy=cart-ingredients] li').should('have.length', 1);
+    //  ожидаем получение булки в корзине
+    cy.get('[data-cy=cart-buns]').should('have.length', 1);
   });
 });
